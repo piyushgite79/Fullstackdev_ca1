75943e4e7f65fa13c6778e45e8892c24875d9bdc
piyushgite79 <psgite96@gmail.com>
2025-03-04 14:17:04 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ServiceCard from './components/ServiceCard'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <ServiceCard title={"Web Development"} description={"We Create proffesional and responsive websites"}/>
+    </div>
+  )
+}
+
+export default App
